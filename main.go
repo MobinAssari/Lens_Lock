ee670be8cdbccb173c61efc2e6bfa0b7dc9927d4
Mobin <mobin.aranii@gmail.com>
2024-05-29 20:31:20 +0330

    add router and modd.conf

--- main.go
+++ main.go
@@ -5,11 +5,44 @@ import (
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1> welcom to my site</h1>")
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-7")
+	fmt.Fprint(w, "<h1> welcome to my site</h1>")
 }
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-7")
+	fmt.Fprint(w, "<h1>this is contact page</h1><p>To get in touch email me at <a href= \"milto:mobin.com\">mmmobin.com</a>")
+}
+
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+
+	}
+}
+
+type Router struct{}
+
+func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+
+	}
+}
+
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	fmt.Println("staring on :3000...")
-	http.ListenAndServe(":3000", nil)
+	var router Router
+	fmt.Println("starting on :3000...")
+	http.ListenAndServe(":3000", router)
 }
